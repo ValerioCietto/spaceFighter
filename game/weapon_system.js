ff89581ac686d0f2fbf6841d79b2be95ccbfb746
siluxmediaPC <siluxmedia@gmail.com>
2025-12-21 00:52:35 +0100

    more weapons

--- game/weapon_system.js
+++ game/weapon_system.js
@@ -1,3 +1,114 @@
+
+ // Weapons definitions
+      const WeaponSpaceBullet = {
+        name: "Space Bullet",
+        damage: 1,
+        base_speed: 250,
+        life_span: 3.0,
+        spread: 0.15,
+        projectiles: 1,
+        aspect: "round_bullet",
+        delay_ms: 50
+      };
+
+      const WeaponSniper = {
+        name: "Sniper",
+        damage: 5,
+        base_speed: 1000,
+        life_span: 5.0,
+        spread: 0.0,
+        projectiles: 1,
+        aspect: "line",
+        auto_aim: 0.5,
+        delay_ms: 300
+      };
+
+      const WeaponShotgun = {
+        name: "Shotgun",
+        damage: 2,
+        base_speed: 300,
+        acceleration: -75,
+        life_span: 2.0,
+        spread: 10.0,
+        projectiles: 5,
+        aspect: "bullet",
+        delay_ms: 200
+      };
+
+      const WeaponHomingMissiles = {
+        name: "Homing missiles",
+        damage: 5,
+        base_speed: 100,
+        acceleration: 10,
+        speed: 300,
+        turning_speed_deg: 90,
+        life_span: 10.0,
+        spread: 3.0,
+        projectiles: 1,
+        aspect: "missile",
+        delay_ms: 1000,
+        homing: true
+      };
+      WeaponHomingMissiles.turn_speed_rad =
+        WeaponHomingMissiles.turning_speed_deg * Math.PI / 180;
+
+      const WeaponLazyMissiles = {
+        name: "Lazy missiles",
+        damage: 10,
+        base_speed: 10,
+        acceleration: 300,
+        speed: 1000,
+        turning_speed_deg: 10,
+        life_span: 10.0,
+        spread: 3.0,
+        projectiles: 2,
+        aspect: "missile",
+        delay_ms: 500,
+        homing: true
+      };
+      WeaponLazyMissiles.turn_speed_rad =
+        WeaponLazyMissiles.turning_speed_deg * Math.PI / 180;
+
+      const WeaponShockwave = {
+        name: "Shockwave",
+        damage: 2,
+        base_speed: 300,
+        acceleration: -75,
+        life_span: 0.2,
+        spread: 360.0,
+        projectiles: 90,
+        aspect: "line",
+        delay_ms: 200
+      };  
+
+      const WeaponMinelayer = {
+        name: "Minelayer",
+        damage: 1,
+        base_speed: 2,
+        acceleration: -250,
+        life_span: 120.0,
+        speed: 0,
+        spread: 360.0,
+        projectiles: 30,
+        aspect: "bullet",
+        delay_ms: 2000
+      }
+
+
+      const weapons = [
+        WeaponLazyMissiles,
+        WeaponMinelayer,
+        WeaponSpaceBullet,
+        WeaponSniper,
+        WeaponShotgun,
+        WeaponHomingMissiles,
+        WeaponShockwave
+      ];
+      let currentWeaponIndex = 0;
+      const weaponLastFire = [0, 0, 0, 0, 0,0,0];
+
+
+
 function normalizeAngleDiff(diff) {
   diff = (diff + Math.PI) % (2 * Math.PI);
   if (diff < 0) diff += 2 * Math.PI;
