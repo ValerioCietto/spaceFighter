68bd0436f324847c91b8bb170360822ac52b5be9
siluxmediaPC <siluxmedia@gmail.com>
2025-12-20 20:26:28 +0100

    now we can work parallel with local and web dev

---
+++ game/const.js
@@ -0,0 +1,5 @@
+// the purpose of this file is to contains the constants
+// so they will be available to all successive modules.
+
+// image and assets path
+window.BASE_PATH = location.hostname.includes("github.io") ? "/spaceFighter" : "";
