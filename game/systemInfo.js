7493fc93f89608e87dc9546887dcb0c332b2aab2
siluxmediaPC <siluxmedia@gmail.com>
2026-02-24 23:59:21 +0100

    easier start!

--- game/systemInfo.js
+++ game/systemInfo.js
@@ -1,9 +1,9 @@
 const SystemInfo = {
-    name: "Sol",
+    name: "Glacius",
     size: 6000,
     stars: [
         {
-            name: "Sun",
+            name: "Glacius Prime",
             position_x: 3000,
             position_y: 3000,
             radius: 100
@@ -12,49 +12,17 @@ const SystemInfo = {
     planets: [],
     stations: [
         {
-            name: "Terra",
+            name: "Brrrr Station",
             position_x: 3450,
             position_y: 2800,
             station_radius: 80,
             station_rot_speed: Math.PI / 32
         }
     ],
-    hyperspace_gates: [
-        {
-            name: "Greenfield",
-            position_x: 3200,
-            position_y: 200,
-            rotation: 0.1,
-            width: 250,
-            type: "warp"
-        },
-        {
-            name: "Eos",
-            position_x: 5100,
-            position_y: 3000,
-            rotation: 0.1,
-            width: 250,
-            type: "warp"
-        },
-                {
-            name: "Alpha Centauri",
-            position_x: 3700,
-            position_y: 5200,
-            rotation: 0.1,
-            width: 250,
-            type: "warp"
-        },
-        {
-            name: "Glacius",
-            position_x: 400,
-            position_y: 5000,
-            rotation: 0.1,
-            width: 250,
-            type: "warp"
-        }
+    hyperspace_gates: [   
     ],
-    max_enemy_number:2,
-    spawn_rate:4, // threat level
+    max_enemy_number:0,
+    spawn_rate:1, // threat level
 };
 // on window load, fetch galaxy-map.json and populate SystemInfo.hyperspace_gates with the data, then export SystemInfo
 window.addEventListener("load", () => {
